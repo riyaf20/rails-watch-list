0136f5bc54803ac5e73fa225aaebb780c629ee38
Riya Fartyal <riyaf3105@gmail.com>
2024-05-10 13:28:49 +0900

    Fixing errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -73,4 +73,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # config/environments/development.rb
+# ⚠️ MAKE SURE YOU REMOVE THE EXISTING ONE
+# config.active_storage.service = :local
+# Add this one 👇
+  config.active_storage.service = :cloudinary
+
 end
